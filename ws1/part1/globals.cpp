74e810a658f6186cb0e6a90b5f29a5be478abf2f
Intae Chung <itchung@myseneca.ca>
2023-09-19 13:56:07 -0400

    updating git

--- ws1/part1/globals.cpp
+++
@@ -1,4 +0,0 @@
-#include "globals.h"
-
-double g_taxrate = 0.0; // Initialize with default values
-double g_discount = 0.0; // Initialize with default values
--- ws1/part1/globals.h
+++
@@ -1,7 +0,0 @@
-#ifndef SDDS_GLOBALS_H
-#define SDDS_GLOBALS_H
-
-extern double g_taxrate;
-extern double g_discount;
-
-#endif // SDDS_GLOBALS_H
