8cf52f0f79f9a0d977a06a65831409bee52e4504
Intae Chung <itchung@myseneca.ca>
2023-10-04 16:58:39 -0400

    constructor fixed

--- ws3/Book.cpp
+++ ws3/Book.cpp
@@ -9,22 +9,28 @@
 
 namespace sdds {
     Book::Book() {};
-    Book::Book(const std::string& title, unsigned numChapters, unsigned numPages) {};
+    Book::Book(const std::string& title, unsigned numChapters, unsigned numPages) : m_title(title), m_numChapters(numChapters), m_numPages(numPages) {};
 
     Book::operator bool()const {
-        return m_title.empty() || m_numChapters == 0 || m_numPages == 0;
+        return m_title[0] != '\0' && m_numChapters > 0 && m_numPages > 0;
     }
 
     std::ostream& Book::print(std::ostream &os)const {
-        if (!this) {
-            os << std::right;
-            os.width(56);
-            os << m_title << ',' << m_numChapters 
-            << ',' << m_numPages << " | (";
+        if (*this) {
+            std::string titleField = m_title + ',' + std::to_string(m_numChapters) + ',' + std::to_string(m_numPages);
+            titleField = std::string(56 - titleField.length(), ' ') + titleField;
+            // os << std::right;
+            // os.width(56);
+            // os << m_title << ',' << m_numChapters 
+            // << ',' << m_numPages << " | (";
 
             double avgPagesPerChapter = static_cast<double>(m_numPages) / m_numChapters;
-            os.width(15);
-            os << std::left << std::fixed << std::setprecision(6) << avgPagesPerChapter << ")    "; 
+            std::string avgField = " | (" + std::to_string(avgPagesPerChapter) + ')';
+            avgField = avgField.substr(1, 15);
+
+            os << titleField << avgField << "    ";
+            // os.width(15);
+            // os << std::left << std::fixed << std::setprecision(6) << avgPagesPerChapter << ")    "; 
         } else {
             os << "| Invalid book data";
         }
