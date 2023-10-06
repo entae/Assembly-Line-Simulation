3941a358eafafd1c80a3272374bc70279c6442ea
Intae Chung <itchung@myseneca.ca>
2023-10-06 01:18:35 -0400

    w4p1

---
+++ ws4/Cheese.cpp
@@ -0,0 +1,97 @@
+#include <iomanip>
+#include "Cheese.h"
+
+namespace sdds {
+
+    Cheese::Cheese(const std::string &desc) {
+        if (!desc.empty()) {
+            //temp string to safely manipulate input string after removing lead/ending spaces
+            size_t startPos = desc.find_first_not_of(" ");
+            size_t endPos = desc.find_last_not_of(" ");
+            std::string descCpy = desc.substr( startPos, (endPos - startPos + 1) );
+
+            // m_name
+            size_t comma = descCpy.find(',');
+            m_name = descCpy.substr( 0, comma );
+            if (comma != std::string::npos) descCpy.erase( 0, comma+1 );
+
+            // m_weight
+            comma = descCpy.find(',');
+            m_weight = std::stoul( descCpy.substr( 0, comma ) );
+            if (comma != std::string::npos) descCpy.erase( 0, comma+1 );
+            
+
+            // m_price
+            comma = descCpy.find(',');
+            m_price = std::stod( descCpy.substr( 0, comma ) );
+            if (comma != std::string::npos) descCpy.erase( 0, comma+1 );
+
+            // m_desc
+            char target = ',';
+            char replacement = ' ';
+            for (size_t i = 0; i < descCpy.length(); ++i) {
+                if (descCpy[i] == target) {
+                    descCpy[i] = replacement;
+                }
+            }
+            // Reduce consecutive spaces to a single space
+            for ( size_t i = 0; i < descCpy.length() - 1; ++i) {
+                if (descCpy[i] == ' ' && descCpy[i + 1] == ' ') {
+                    size_t j = i + 1;
+                    while (j < descCpy.length() && descCpy[j] == ' ') {
+                        descCpy.erase(j, 1);
+                    }
+                }
+            }
+            m_desc = descCpy.substr( 0, descCpy.length() );
+
+        } else {
+            m_name = nullptr;
+            m_weight = 0;
+            m_price = 0.0;
+            m_desc = nullptr;
+        }
+    }
+
+    Cheese Cheese::slice(size_t weight){
+        Cheese temp = *this;
+        if (weight <= temp.m_weight) {
+            temp.m_weight -= weight;
+        } else {
+            static Cheese empty;
+            temp = empty;
+        }
+        return temp;
+    }
+
+    std::string Cheese::getName()const {
+        return m_name;
+    }
+    size_t Cheese::getWeight()const {
+        return m_weight;
+    }
+    double Cheese::getPrice()const {
+        return m_price;
+    }
+    std::string Cheese::getDesc()const {
+        return m_desc;
+    }
+
+    // |[Name;left;w21]|[weight; ;w5]|[price;fixed;precision2;w5]|[desc;right;w34]|\n
+    std::ostream& operator<<(std::ostream& os, const Cheese& chez) {
+        os << '|';
+        os.width(21);
+        os << std::left << chez.getName() << '|';
+
+        os.width(5);
+        os << chez.getWeight() << '|';
+
+        os.width(5);
+        os << std::fixed << std::setprecision(2) << chez.getPrice() << '|';
+
+        os.width(33);
+        os << std::right << chez.getDesc() << " |" << std::endl;
+
+        return os;
+    }
+}
---
+++ ws4/Cheese.h
@@ -0,0 +1,53 @@
+// In Tae Chung | 128 958 220
+// itchung@myseneca.ca
+// Oct 1, 2023
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
+#ifndef SDDS_CHEESE_H
+#define SDDS_CHEESE_H
+
+#include <iostream>
+
+namespace sdds {
+
+    //block of cheese that can be sliced into smaller cheeses
+    class Cheese {
+        std::string m_name{ "NaC" }; //defaults to NaC "Not a Cheese"
+        size_t m_weight{};//in grams
+        double m_price{}; //price per gram
+        std::string m_desc{}; //features or description of cheese
+    public:
+        Cheese()=default;
+
+        // decipher desc string and initialize as attributes
+        // format: Name,Weight,Price,Feature1,Feature2,etc..
+        // m_desc: "Hard Sharp Smooth Smelly"
+        // remove all leading and trailing spaces from beg-end
+        //consider:
+            //std::string::substr()
+            // std::string::find()
+            // std::string::find_first_of()
+            // std::string::erase()
+            // std::stoul()
+            // std::stod()
+        Cheese(const std::string& desc); //receives m_desc
+        
+        // valid: return copy of current cheese with desired weight
+        // else: return cheese object that is an empty state
+        Cheese slice(size_t weight); //amount to slice off
+
+        //set of queries
+        std::string getName()const;
+        size_t getWeight()const;
+        double getPrice()const;
+        std::string getDesc()const;
+
+        //operator bool()const;
+    };
+
+    //free helper
+    // |[Name;left;w21]|[weight; ;w5]|[price;fixed;precision2;w5]|[desc;right;w34]|\n
+    std::ostream& operator<<(std::ostream& os, const Cheese& chez);
+
+}
+#endif //!SDDS_CHEESE_H
