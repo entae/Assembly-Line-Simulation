676d92775cbc5a76b96779e5cbd6cee57b78ebd7
Intae Chung <itchung@myseneca.ca>
2023-10-11 05:55:01 -0400

    w4p2 test

--- ws4/CheeseShop.cpp
+++ ws4/CheeseShop.cpp
@@ -50,7 +50,7 @@ namespace sdds {
     CheeseShop& CheeseShop::operator=(CheeseShop&& chez) {
         //std::cout << "MOVING!!" << std::endl;
         if (this != &chez) {
-            clear();
+            //clear();
             m_shopName = std::move(chez.m_shopName);
             m_numCheeses = std::move(chez.m_numCheeses);
             m_cheeses = std::move(chez.m_cheeses);
