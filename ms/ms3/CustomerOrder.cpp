2c7e40e23612322d81c5f3f0c467c1c3b9b4ae4c
Intae Chung <itchung@myseneca.ca>
2023-12-05 01:31:50 -0500

    fixed CustomerOrder display

--- ms/ms3/CustomerOrder.cpp
+++ ms/ms3/CustomerOrder.cpp
@@ -129,8 +129,8 @@ namespace sdds {
     void CustomerOrder::display(std::ostream& os) const {
         os << m_name << " - " << m_product << std::endl;
         for (size_t i = 0; i < m_cntItem; i++) {
-            os << "[" << std::setw(6) << std::setfill('0') << m_lstItem[i]->m_serialNumber << "] "
-                << std::setw(m_widthField) << std::setfill(' ') << m_lstItem[i]->m_itemName << " - ";
+            os << std::right << "[" << std::setw(6) << std::setfill('0') << m_lstItem[i]->m_serialNumber << "] "
+                << std::left << std::setw(m_widthField) << std::setfill(' ') << m_lstItem[i]->m_itemName << " - ";
             if (m_lstItem[i]->m_isFilled) {
                 os << "FILLED";
             } else {
